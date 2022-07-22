af9eb8e58777ebb1c16540883977fa2476781d30
Robert Csakany <robert.csakany@blackbelt.hu>
2022-07-22 16:20:39 +0200

    Removing unused API methods

--- src/main/java/hu/blackbelt/judo/dao/api/PayloadValidator.java
+++ src/main/java/hu/blackbelt/judo/dao/api/PayloadValidator.java
@@ -7,19 +7,7 @@ import org.eclipse.emf.ecore.EReference;
 import java.util.*;
 
 public interface PayloadValidator {
-
-    Collection<ValidationResult> validateInsert(final EClass transferObjectType, Payload input);
-
-    Collection<ValidationResult> validateUpdate(final EClass transferObjectType, Payload input);
-
-    Collection<ValidationResult> validateSetReference(final EClass transferObjectType, Payload input);
-
-    Collection<ValidationResult> validateUnsetReference(final EClass transferObjectType, Payload input);
-
-    Collection<ValidationResult> validateAddReference(final EClass transferObjectType, Payload input);
-
-    Collection<ValidationResult> validateRemoveReference(final EClass transferObjectType, Payload input);
-
+    
     Collection<ValidationResult> validatePayload(final EClass transferObjectType, final Payload input, final Map<String, Object> validationContext, boolean throwValidationException);
 
     Collection<ValidationResult> validateReference(final EReference reference, final Payload instance, final Map<String, Object> validationContext, final boolean ignoreInvalidValues);
