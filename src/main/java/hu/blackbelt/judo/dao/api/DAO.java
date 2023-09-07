b7d6c93350d3e572eeefff92ec77d885fcf7a160
Madmat8 <Mateka6400@gmail.com>
2023-09-07 09:22:11 +0200

    fix docs

--- src/main/java/hu/blackbelt/judo/dao/api/DAO.java
+++ src/main/java/hu/blackbelt/judo/dao/api/DAO.java
@@ -74,7 +74,7 @@ public interface DAO<ID> {
      * @param reference       transfer objet relation
      * @param payload         owner data of relation
      * @param queryCustomizer query customizer (i.e. filtering, ordering, seeking)
-     * @param stateful allow create a new instance or update the existing instance of the 'payload'
+     * @param stateful        allow create a new instance or update the existing instance of the 'payload'
      * @return list of possible item(s)
      */
     Collection<Payload> getRangeOf(EReference reference, Payload payload, QueryCustomizer<ID> queryCustomizer, boolean stateful);
@@ -85,7 +85,7 @@ public interface DAO<ID> {
      * @param reference       transfer objet relation
      * @param payload         owner data of relation
      * @param queryCustomizer query customizer (i.e. filtering, ordering, seeking)
-     * @param stateful allow create a new instance or update the existing instance of the 'payload'
+     * @param stateful        allow create a new instance or update the existing instance of the 'payload'
      * @return number of possible item(s)
      */
     long countRangeOf(EReference reference, Payload payload, QueryCustomizer<ID> queryCustomizer, boolean stateful);
